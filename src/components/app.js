c01cfa206c36f7af56ec373d75b1acd4df736144
Susana Paracuellos <82464797+tecnosue@users.noreply.github.com>
2022-10-10 19:00:45 +0200

    generated project

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>
+          logo
+        </h1>
+        
+      </div>
+    );
+  }
+}
