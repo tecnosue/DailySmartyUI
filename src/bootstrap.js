3ea1d788513281a34e248f302772128e00a60473
Susana Paracuellos <82464797+tecnosue@users.noreply.github.com>
2022-10-14 02:48:20 +0200

    algun error tipograficp

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,31 +1,31 @@
-import  React from 'react';
+import React from "react";
 
-import  ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import reducers from './reducers';
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+import "./style/main.scss";
 
-import './style/main.scss';
-
-import Home from './components/home';
-import Results from './components/results';
+import Home from "./components/home";
+import Results from "./components/results";
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route path='/results' componet={Results}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/results" component={Results} />
         </Switch>
       </BrowserRouter>
-    </Provider>
-    , document.querySelector('.app-wrapper'));
+    </Provider>,
+    document.querySelector(".app-wrapper")
+  );
 }
 
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener("DOMContentLoaded", main);
--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -6,11 +6,11 @@ class RecentPosts extends Component {
       <div className="recent-post">
         <div className="recent-post__wrapper">
           <div className="recent-post__heading">Recent Post</div>
-          <ul className="recent-post__post">
-            <li>recent post 0</li>
-            <li>recent post 1</li>
-            <li>recent post 2</li>
-          </ul>
+            <ul className="recent-post__post">
+              <li>recent post 0</li>
+              <li>recent post 1</li>
+              <li>recent post 2</li>
+            </ul>
         </div>
       </div>
     );
