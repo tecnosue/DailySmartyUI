8c2c11c07810ab40adc34f29e19489688cdb260a
Susana Paracuellos <82464797+tecnosue@users.noreply.github.com>
2022-10-19 18:41:27 +0200

    implemented font awesome into searchbar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -9,7 +9,7 @@ class SearchBar extends Component {
   }
 
   renderInput(field) {
-    return <input type="text" placeholder="Search DailySmarty" {...field.input} />
+    return <input type="text" placeholder="&#xf002;Search DailySmarty" {...field.input} />
     
   }
 
